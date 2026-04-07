1d89f8d70dfc45669726af9fe41c03555f4aec53
THAMARAICHANDHIRAN G <thamaraisrikar@gmail.com>
2026-04-07 11:15:21 +0530

    Time: 18 ms (27.09%), Space: 60.4 MB (91.13%) - LeetHub

--- 2448-minimum-cost-to-make-array-equal/2448-minimum-cost-to-make-array-equal.java
+++ 2448-minimum-cost-to-make-array-equal/2448-minimum-cost-to-make-array-equal.java
@@ -2,34 +2,39 @@ class Solution {
     public long minCost(int[] nums, int[] cost) {
         int n = nums.length;
 
+
         long[][] arr = new long[n][2];
         for (int i = 0; i < n; i++) {
             arr[i][0] = nums[i];
             arr[i][1] = cost[i];
         }
 
+
         Arrays.sort(arr, (a, b) -> Long.compare(a[0], b[0]));
 
-        long totalCost = 0;
+   
+        long totalWeight = 0;
         for (int i = 0; i < n; i++) {
-            totalCost += arr[i][1];
+            totalWeight += arr[i][1];
         }
 
-        long curr = 0;
+      
+        long cumulativeWeight = 0;
         long target = 0;
-
         for (int i = 0; i < n; i++) {
-            curr += arr[i][1];
-            if (curr >= (totalCost + 1) / 2) {
+            cumulativeWeight += arr[i][1];
+            if (cumulativeWeight >= (totalWeight + 1) / 2) {
                 target = arr[i][0];
                 break;
             }
         }
+
+
         long ans = 0;
         for (int i = 0; i < n; i++) {
-            ans += Math.abs(nums[i] - target) * (long)cost[i];
+            ans += Math.abs((long)nums[i] - target) * (long)cost[i];
         }
+        
         return ans;
-
     }
 }
