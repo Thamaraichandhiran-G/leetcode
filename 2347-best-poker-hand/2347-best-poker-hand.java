dce91abac47005e82752ae424c274a215d94745c
THAMARAICHANDHIRAN G <thamaraisrikar@gmail.com>
2026-05-19 11:19:49 +0530

    Time: 4 ms (5.35%), Space: 43 MB (35.8%) - LeetHub

---
+++ 2347-best-poker-hand/2347-best-poker-hand.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String bestHand(int[] ranks, char[] suits) {
+        Map<Integer,Integer> rk = new HashMap<>();
+        Map<Character,Integer> su = new HashMap<>();
+
+        for(int  i: ranks){
+            rk.put(i,rk.getOrDefault(i,0)+1);
+        }
+        System.out.println(rk);
+        for(char c: suits){
+            su.put(c,su.getOrDefault(c,0)+1);
+        }
+        if(5==su.get(suits[0])) return "Flush";
+
+       for(int j : rk.values()){
+            if(j>=3){
+                return "Three of a Kind";
+            }
+        }
+        for(int j : rk.values()){
+            if(j>=2){
+                return "Pair";
+            }
+        }
+        return "High Card";
+    }
+}
