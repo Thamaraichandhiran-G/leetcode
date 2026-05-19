697a80db00e1016ed5792f946acc1c4ee0081c76
THAMARAICHANDHIRAN G <thamaraisrikar@gmail.com>
2026-05-19 11:45:50 +0530

    Time: 3 ms (7.41%), Space: 43.2 MB (6.58%) - LeetHub

--- 2347-best-poker-hand/2347-best-poker-hand.java
+++ 2347-best-poker-hand/2347-best-poker-hand.java
@@ -12,16 +12,25 @@ class Solution {
         }
         if(5==su.get(suits[0])) return "Flush";
 
-       for(int j : rk.values()){
-            if(j>=3){
-                return "Three of a Kind";
-            }
+    //    for(int j : rk.values()){
+    //         if(j>=3){
+    //             return "Three of a Kind";
+    //         }
+    //     }
+    //     for(int j : rk.values()){
+    //         if(j>=2){
+    //             return "Pair";
+    //         }
+    //     }
+        int max = Collections.max(rk.values());
+        if(max>=3){
+            return "Three of a Kind";
         }
-        for(int j : rk.values()){
-            if(j>=2){
-                return "Pair";
-            }
+        else if( max ==2 ){
+            return "Pair";
         }
-        return "High Card";
+        else{
+            return "High Card";
+        }
     }
 }
