5280a43b642c675fda5e0c2bbd431275d813ac08
THAMARAICHANDHIRAN G <thamaraisrikar@gmail.com>
2026-05-27 09:51:21 +0530

    Time: 15 ms (5.14%), Space: 46.4 MB (19.18%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.java
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -15,18 +15,35 @@ class Solution {
         // return -1;
 
         int n = arr.length;
-        int ans=0;
-        int c=0;
-        for(int i=0;i<32;i++){
-            c=0;
-            for(int j=0;j<n;j++){
-                c+= arr[j] & 1;
-                arr[j]= arr[j]>>1;
+        // int ans=0;
+        // int c=0;
+        // for(int i=0;i<32;i++){
+        //     c=0;
+        //     for(int j=0;j<n;j++){
+        //         c+= arr[j] & 1;
+        //         arr[j]= arr[j]>>1;
+        //     }
+        //     if(c%3==1){
+        //         ans=ans|(1<<i);
+        //     }
+        // }
+        // return ans;
+
+        if(n<=3){
+            return arr[0];
+        }
+
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+
+        for(int i=1;i<n;i+=3){
+            if(arr[i-1]==arr[i]){
+                continue;
             }
-            if(c%3==1){
-                ans=ans|(1<<i);
+            else{
+                return arr[i-1];
             }
         }
-        return ans;
+        return arr[n-1];
     }
 }
