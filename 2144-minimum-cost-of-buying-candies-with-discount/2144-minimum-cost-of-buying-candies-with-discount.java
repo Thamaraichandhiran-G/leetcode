b96b5395fe7cb0afb64a1814671699ff0c070a12
THAMARAICHANDHIRAN G <thamaraisrikar@gmail.com>
2026-06-01 09:41:05 +0530

    Time: 6 ms (41.78%), Space: 44.5 MB (69.64%) - LeetHub

--- 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.java
+++ 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.java
@@ -7,13 +7,13 @@ class Solution {
         int c=0;
 
         int n = arr.length;
-        if(n==1){
-            return arr[0];
-        }
-        if(n==2){
-            c=arr[0]+arr[1];
-            return c;
-        }
+        // if(n==1){
+        //     return arr[0];
+        // }
+        // if(n==2){
+        //     c=arr[0]+arr[1];
+        //     return c;
+        // }
         int i=n-1;
         for(i=n-1;i>=0;i=i=i-3){
             c+=arr[i];
