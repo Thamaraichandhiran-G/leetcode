f266781b67bf517a427a3fe1986c1c0e93346b5c
THAMARAICHANDHIRAN G <thamaraisrikar@gmail.com>
2026-06-22 16:03:01 +0530

    Time: 2 ms (29.2%), Space: 45.5 MB (48.27%) - LeetHub

--- 0443-string-compression/0443-string-compression.java
+++ 0443-string-compression/0443-string-compression.java
@@ -24,7 +24,6 @@ class Solution {
         for(int i=0;i<sb.length();i++){
             chars[i]=sb.charAt(i);
         }
-        System.out.println(sb.length());
         return sb.length();
         
     }
