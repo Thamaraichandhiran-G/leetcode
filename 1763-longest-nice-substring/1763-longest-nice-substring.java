5b531ac0cf8192e65a0657c031e627b0f3dfc451
THAMARAICHANDHIRAN G <thamaraisrikar@gmail.com>
2026-06-24 10:28:28 +0530

    Time: 71 ms (8.27%), Space: 47.1 MB (6.85%) - LeetHub

---
+++ 1763-longest-nice-substring/1763-longest-nice-substring.java
@@ -0,0 +1,39 @@
+class Solution {
+    public String longestNiceSubstring(String s) {
+        String ans = "";
+
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i + 1; j <= s.length(); j++) {
+                String sub = s.substring(i, j);
+
+                if (isNice(sub) && sub.length() > ans.length()) {
+                    ans = sub;
+                }
+            }
+        }
+
+        return ans;
+    }
+
+    private boolean isNice(String s) {
+        java.util.Set<Character> set = new java.util.HashSet<>();
+
+        for (char c : s.toCharArray()) {
+            set.add(c);
+        }
+
+        for (char c : s.toCharArray()) {
+            if (Character.isLowerCase(c)) {
+                if (!set.contains(Character.toUpperCase(c))) {
+                    return false;
+                }
+            } else {
+                if (!set.contains(Character.toLowerCase(c))) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
