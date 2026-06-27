3eb6e9cba0863a194d99e6447c3a2cc15d99b788
THAMARAICHANDHIRAN G <thamaraisrikar@gmail.com>
2026-06-27 09:25:00 +0530

    Time: 3 ms (52.7%), Space: 44 MB (43.93%) - LeetHub

---
+++ 3541-find-most-frequent-vowel-and-consonant/3541-find-most-frequent-vowel-and-consonant.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxFreqSum(String s) {
+        
+        String v = "aeiou";
+        int maxv=0, maxc=0;
+        HashMap<Character,Integer> map = new HashMap<>();
+        for(char c: s.toCharArray()){
+            map.put(c,map.getOrDefault(c,0)+1);
+        }
+        for(char c: map.keySet()){
+            if(v.indexOf(c)==-1){
+                maxv= Math.max(maxv,map.get(c));
+            }else{
+                maxc= Math.max(maxc,map.get(c));
+            }
+        }
+        return maxv+maxc;
+    }
+}
