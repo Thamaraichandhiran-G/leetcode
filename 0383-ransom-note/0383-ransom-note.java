ad7ea25cff233160bf53d482b8b40ad8588938d1
THAMARAICHANDHIRAN G <thamaraisrikar@gmail.com>
2026-06-27 09:42:42 +0530

    Time: 1 ms (99.96%), Space: 45.9 MB (98.77%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean canConstruct(String r, String m) {
+        
+        int arr[]= new int[26];
+
+        for(char c: m.toCharArray()){
+            arr[c-'a']++;
+        }
+        
+        for(char c: r.toCharArray()){
+            arr[c-'a']--;
+            if(arr[c-'a']<0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
